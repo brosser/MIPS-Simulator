4fddfe08b2fde9ddad7da59aebbe550e44082564
Fredrik <fb.msngr@gmail.com>
2013-02-18 21:03:38 +0800

    Add preprocessing log

--- run-simulator.py
+++ run-simulator.py
@@ -15,10 +15,14 @@ def main() :
 	
 	#print lines
 	
-	filename = sys.argv[2] if len(sys.argv) > 2 else "debug.txt"
-	f = open(filename, 'w')
+	simulationFile = sys.argv[2] if len(sys.argv) > 2 else "simrun.txt"
+	sF = open(simulationFile, 'w')
+	preprocessingLog = sys.argv[3] if len(sys.argv) > 3 else "preprocLog.txt"
+	ppL = open(preprocessingLog, 'w')
 	sys.stdout = f
 	pipelinesim.run()
+	sF.close()
+	ppL.close()
 
 if __name__ == "__main__":
     main()
