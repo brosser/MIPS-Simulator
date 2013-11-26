a61761e841a287e2165f6d14aefa7bcc3bdad7ca
Hui Yan Cheah <hycheah@gmail.com>
2013-11-26 14:36:11 +0800

    function called two times

--- benchmark/toy-llvm-mips/add/add.c
+++ benchmark/toy-llvm-mips/add/add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static int output = 30;
+static int output = 80;
 
 int add (int i, int j) {
 	return i + j;
@@ -10,11 +10,13 @@ int main () {
 	
 	int a = 10;
 	int b = 20;
+	int c = 30;
 	int main_result = 0;
 
-	int answer = add(a,b);
+	int answer1 = add(a, b); // 10 + 20 = 30
+	int answer2 = answer1 + add(b ,c); // 30 + 20 + 30 = 80
 
-	main_result = (answer != output);
+	main_result = (answer2 != output);
 	//printf("%d\n", main_result);
 
 return main_result;
