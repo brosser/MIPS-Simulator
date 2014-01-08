45591ce5a8b1f67a80c1e8b4061db166e75358fb
Hui Yan Cheah <hycheah@gmail.com>
2014-01-08 12:38:08 +0800

    reduced size

--- benchmark/toy-llvm-mips/knapsack/knapsack.c
+++ benchmark/toy-llvm-mips/knapsack/knapsack.c
@@ -1,17 +1,17 @@
 // The 0-1 knapSack Problem
 #include<stdio.h>
 
-const int output = 2;
+const int output = 5;
 
 int max(int a, int b) { return (a > b)? a : b; }
 
 int knapSack(int W, int wt[], int val[], int n){
 
 	int i, w;
-	int K[n+1][W+1];
+	int K[n+1][W+1]; 
 
-	for (i = 0; i <= n; i++){
-    		for (w = 0; w <= W; w++){
+	for (i = 0; i <= n; i++){ // n =10
+    		for (w = 0; w <= W; w++){ // W = 20
       			if (i==0 || w==0)
         			K[i][w] = 0;
       			else if (wt[i-1] <= w)
@@ -21,7 +21,7 @@ int knapSack(int W, int wt[], int val[], int n){
     		}
   	}
   
-  /*	for (i = 0; i <= n; i++){
+ /* 	for (i = 0; i <= n; i++){
     		for (w = 0; w <= W; w++){
       			printf("%d\t", K[i][w]);
     		}
@@ -34,20 +34,20 @@ int knapSack(int W, int wt[], int val[], int n){
 int main() {
 
 	int i, n, /*val[100], wt[100], */W;
-	int val[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Item values
-	int wt[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 1010}; // Item weights
+	int val[5] = { 1, 2, 3, 4, 5 }; // Item values
+	int wt[5] = { 10, 20, 30, 40, 50}; // Item weights
 	int return_value;
 	int main_result = 0;
 
-	n = 10; // Number of items
-
-	W = 20; // Maximum weight
+	n = 5; // Number of items
+	W = 50; // Maximum weight
 
   	return_value = knapSack(W, wt, val, n);
 	
 	// Check output
 	main_result = (return_value != output);
 
+  	//printf("%d\n", return_value);
   	//printf("%d\n", main_result);
 
 	return main_result;
