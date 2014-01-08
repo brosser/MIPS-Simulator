533820065d1d39b804b3ae57ccfda559c90767d2
Hui Yan Cheah <hycheah@gmail.com>
2014-01-08 10:46:57 +0800

    fiddle: benchmark with pointers

--- benchmark/toy-llvm-mips/fiddle/fiddle.c
+++ benchmark/toy-llvm-mips/fiddle/fiddle.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
-
+// This benchmark has pointers
 // result i = 0, j = 1
 
-void fiddle(int x, int *y)
+const int output[15] = { 0, 2, 4, 1, 3, 5, 2, 4, 6, 3, 5, 7, 4, 6, 8};
+
+int fiddle(int x, int *y)
 {   
+	int a, b;
 	//printf(" Starting fiddle: x = %d, y = %d\n", x, *y);
-    	x ++;
-    	(*y) ++;
-    	//printf("Finishing fiddle: x = %d, y = %d\n", x, *y);
+    	a = x++;
+    	b = (*y) ++; // if pass in by pointer, the value of y stays modified.
+    	//printf("Finishing fiddle: a = %d, b = %d ", a, b);
+	return (a + b);
 }
 
 int main()
 {   
-	int i = 0;
-    	int j = 0;
+	int i;
+    	int j;
+	int x = 5;
+	int y = 5;
 	int main_result = 0;
+	int array[15];
+	int n = 0;
 
-    	//printf(" Starting main  : i = %d, j = %d\n", i, j);
-    	//printf("Calling fiddle now\n");
-    	fiddle(i, &j);
-    	//printf("Returned from fiddle\n");
-    	//printf("Finishing main  : i = %d, j = %d\n", i, j);
+    	for (i = 0; i < x; i++) {
+		for (j = 0; j < y; j++){
+			//printf("%d %d\n", i, j); -- value of y gets overwritten in fiddle functions
+			array[n] = fiddle(i, &j);
+			//printf("%d, %d, array %d\n", i, j, array[n]);
+			n = n + 1;
+		}
+	}
 
-	main_result = (i != 0) && (j != 1);
+	for (i = 0; i < n; i++) 
+		main_result += (output[i] != array[i]);
 	
 	//printf("%d\n", main_result);
 
