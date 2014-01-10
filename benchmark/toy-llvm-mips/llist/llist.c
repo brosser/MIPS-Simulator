37047a37349b456bb85bd4d11eaf1f4e2301bbc0
Hui Yan Cheah <hycheah@gmail.com>
2014-01-10 18:15:22 +0800

    new benchmarks

---
+++ benchmark/toy-llvm-mips/llist/llist.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tests a linked list node struct
+// with struct pointers
+
+struct record {
+   volatile struct record * next;
+   short a, b, c, d;
+};
+
+volatile struct record tail = {NULL, 1, 2, 3, 4};
+
+int main()
+{
+   volatile struct record node3 = {&tail, 2, 5, 7, 34};
+   volatile struct record node2 = {&node3, 34, 35, 27, 14};
+   volatile struct record node1 = {&node2, 12, 54, 23, 334};
+   volatile struct record head = {&node1, 24, 245, 537, 3434};
+   int result = 0;
+   volatile struct record * ptr = &head;
+   int main_result = 0;
+
+   while (ptr) {
+       result += ptr->a + ptr->b + ptr->c + ptr->d;
+       ptr = ptr->next;
+   }
+
+   //printf("Result: %d\n", result);
+   if (result == 4831) {
+       //printf("RESULT: PASS\n");
+       main_result = 0;
+   } else {
+       //printf("RESULT: FAIL\n");
+       main_result = 1;
+   }
+  
+   //printf("%d\n", main_result);
+   return main_result;
+}
+
---
+++ benchmark/toy-llvm-mips/shift/shift.c
@@ -0,0 +1,43 @@
+// Left-shifting an integer by more than 32 bits
+// Expected result: 496
+//
+// 1 << 200 
+// = 1 << (200 % 32) 
+// = 1 << 8
+// = 256
+// Taken from motion CHStone benchmark:
+// 4042285200 >> 4294967128
+// = 4042285200 >> (4294967128 % 32) 
+// = 240
+
+#include <stdio.h>
+
+int main() {
+    volatile unsigned int test = 1;
+    volatile unsigned int N = 200;
+    volatile int result = test << N;
+    int main_result = 0;
+
+    //printf("1 << 200 = %d\n", result);
+    //printf("1 << (200 %% 32) = %d\n", test << (N % 32));
+
+    test = 4042285200u;
+    N = 4294967128u;
+
+    volatile int result2 = test >> N;
+    //printf("%u >> %u = %u\n", test, N, result2);
+
+    volatile int result_final = result + result2;
+
+    //printf("Result: %d\n", result_final);
+    if (result_final == 496) {
+        //printf("RESULT: PASS\n");
+	main_result = 0;
+    } else {
+        //printf("RESULT: FAIL\n");
+	main_result = 1;
+    }
+
+    //printf("%d\n", main_result);
+    return main_result;
+}
