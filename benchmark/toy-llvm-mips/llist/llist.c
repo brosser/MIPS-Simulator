f06e1a0deeae76c9d016d2bc4b4284a348e69a39
Hui Yan Cheah <hycheah@gmail.com>
2014-01-11 16:13:17 +0800

    linked-list testbench

--- benchmark/toy-llvm-mips/llist/llist.c
+++ benchmark/toy-llvm-mips/llist/llist.c
@@ -13,9 +13,10 @@ volatile struct record tail = {NULL, 1, 2, 3, 4};
 
 int main()
 {
-   volatile struct record node3 = {&tail, 2, 5, 7, 34};
-   volatile struct record node2 = {&node3, 34, 35, 27, 14};
-   volatile struct record node1 = {&node2, 12, 54, 23, 334};
+   //volatile struct record node3 = {&tail, 2, 5, 7, 34};
+   //volatile struct record node2 = {&node3, 34, 35, 27, 14};
+   //volatile struct record node1 = {&node2, 12, 54, 23, 334};
+   volatile struct record node1 = {&tail, 12, 54, 23, 334};
    volatile struct record head = {&node1, 24, 245, 537, 3434};
    int result = 0;
    volatile struct record * ptr = &head;
@@ -27,7 +28,7 @@ int main()
    }
 
    //printf("Result: %d\n", result);
-   if (result == 4831) {
+   if (result == /*4831*/ 4673) {
        //printf("RESULT: PASS\n");
        main_result = 0;
    } else {
